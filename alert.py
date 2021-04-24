34863cd64b28a8aa50cc3c33d92accca6b8da263
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-04-25 08:23:05 +0900

    first commit

---
+++ alert.py
@@ -0,0 +1,42 @@
+import sys,pymysql.cursors,discord,re,datetime,pytz,time,keys
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
+    while(True):
+        t = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+        wt = (24-t.hour+1)*3600 + t.minute*60
+        time.sleep(wt)
+        conn=pymysql.connect(
+            user = keys.USER,
+            passwd = keys.PW,
+            host = keys.HOST,
+            db = keys.DB,
+            port = keys.PORT,
+        )
+        cursor = conn.cursor()
+        print('successful')
+        now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+        sql = "select * from schedule where (year = " + str(now.year) + " and month =" + str(now.month) + " and day =" + str(now.day) + ")"
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        for row in rows:
+            s = str(row).strip("(")
+            sche = s.strip(")")
+            schedule = sche.split(",",4)
+            channel = client.get_channel(int(schedule[0]))
+            content = schedule[4].split("'",10)
+            await channel.send(content[1])
+            d = "delete from schedule where (id =" + str(schedule[0]) + " and year =" + str(schedule[1]) + " and month =" + str(schedule[2]) + " and day =" + str(schedule[3]) + " and content =" + schedule[4] +")"
+            cursor.execute(d)
+            conn.commit()
+
+        conn.close()
+
+client.run(keys.TK)
---
+++ scheduler.py
@@ -0,0 +1,80 @@
+import sys,pymysql.cursors,discord,re,calendar,keys #keys.pyにdiscordのtokenとSQLデータベースの情報を保存
+
+flag=0
+
+conn=pymysql.connect(
+    user = keys.USER, #str
+    passwd = keys.PW, #str
+    host = keys.HOST, #str
+    db = keys.DB, #str
+    port = keys.PORT, #int
+)
+
+cursor = conn.cursor()
+
+def p_exit():
+    conn.close()
+    sys.exit()
+
+print('successful')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    global flag
+    if message.content.startswith("s_exit"):
+        await message.channel.send("終了します")
+        p_exit()
+    
+    if flag == 0:
+        if message.content.startswith("s_add"):
+            await message.channel.send("`yy.mm.dd.予定`の形式で予定を送信してください")
+            flag = 1
+            return
+        #if message.content.startswith("s_list"):
+
+    
+    if flag == 1:
+        ms_list = message.content.split('.',3)
+
+        if ((len(ms_list) != 4) or (ms_list[2] > 12)):
+            await message.channel.send("もう一度入力してください")
+            return
+        if (ms_list[2] == 1 or ms_list[2] == 3 or ms_list[2] == 5 or ms_list[2] == 7 or ms_list[2] == 8 or ms_list[2] == 9 or ms_list[2] == 12) and (ms_list[3] > 31):
+            await message.channel.send("もう一度入力してください")
+            return
+        if (ms_list[2] == 4 or ms_list[2] == 6 or ms_list[2] == 9 or ms_list[2] == 11) and (ms_list[3] > 30):
+            await message.channel.send("もう一度入力してください")
+            return
+        if (calendar.isleap(ms_list[1])):
+            if(ms_list[2] == 2) and (ms_list > 29):
+                await message.channel.send("もう一度入力してください")
+                return
+        if (ms_list[2] == 2) and (ms_list > 28):
+            await message.channel.send("もう一度入力してください")
+            return
+        
+        channel_id = int(message.channel.id)
+        year = int("20" + ms_list[0])
+        month = int(ms_list[1])
+        day = int(ms_list[2])
+        content = ms_list[3]
+
+        sql = "INSERT INTO schedule(id,year,month,day,content)VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql,(channel_id,year,month,day,content))
+        conn.commit()
+        await message.channel.send(str(year) + "年" + str(month) + "月" + str(day) + "日" + content + "を追加しました")
+        flag = 0
+        return
+
+client.run(keys.TK)
